d42c8297243b8767ecc9efa14c57c2dff946fd73
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-16 17:37:20 +0100

    Add plotter

---
+++ data_plotting/direction/Plot 1/Plot_4LPDA_1dipole_SouthPole.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import time
+
+np.set_printoptions(precision=4)
+
+datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/Alvarez2009_had_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/"
+norm = 1e-6
+
+def spherical_to_cartesian(zenith, azimuth):
+    sinZenith = np.sin(zenith)
+    x = sinZenith * np.cos(azimuth)
+    y = sinZenith * np.sin(azimuth)
+    z = np.cos(zenith)
+    if hasattr(zenith, '__len__') and hasattr(azimuth, '__len__'):
+        return np.array([x, y, z]).T
+    else:
+        return np.array([x, y, z])
+
+
+def load_file(i_file, norm=norm):
+#     t0 = time.time()
+#     print(f"loading file {i_file}", flush=True)
+    data = np.load(os.path.join(datapath, f"data_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+    labels_tmp = np.load(os.path.join(datapath, f"labels_1-3_LPDA_2of4_100Hz_4LPDA_1dipole_fullband_{i_file:04d}.npy"), allow_pickle=True)
+#     print(f"finished loading file {i_file} in {time.time() - t0}s")
+    nu_zenith = np.array(labels_tmp.item()["nu_zenith"])
+    nu_azimuth = np.array(labels_tmp.item()["nu_azimuth"])
+    nu_direction = spherical_to_cartesian(nu_zenith, nu_azimuth)
+#     shower_energy_had.reshape(shower_energy_had.shape[0], 1)
+
+    # check for nans and remove them
+    idx = ~(np.isnan(data))
+    idx = np.all(idx, axis=1)
+    idx = np.all(idx, axis=1)
+    idx = np.all(idx, axis=1)
+    data = data[idx, :, :, :]
+    nu_direction = nu_direction[idx]
+    data /= norm
+#     print(f"finished processing file {i_file} in {time.time() - t0}s")
+
+    return data, nu_direction
---
+++ data_plotting/direction/Plot 1/plotter.py
@@ -0,0 +1,6 @@
+from Plot_4LPDA_1dipole_SouthPole import load_file
+
+data, direction = load_file(1)
+
+print(data.shape)
+print(direction.shape)
