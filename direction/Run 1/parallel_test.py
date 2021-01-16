bc94eed2aa011091253819b54811680a260ade6f
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-16 14:44:20 +0100

    Fix parallel test

--- direction/Run 1/parallel_test.py
+++ direction/Run 1/parallel_test.py
@@ -1,5 +1,10 @@
 import sys
 import time
 print(f"{sys.argv[1]} {sys.argv[2]}")
-time.sleep(5)
+for i in range(5):
+    i = i + 1
+time.sleep(2)
 print(f"Done with {sys.argv[1]}!")
+
+time.sleep(2)
+print(f"Fully Done with {sys.argv[1]}!")
