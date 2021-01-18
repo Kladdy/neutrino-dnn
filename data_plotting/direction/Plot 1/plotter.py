4554a1f245581ced84c8a3a68f96b2ab22b3e319
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-18 09:49:26 +0100

    Add saving of plot

--- data_plotting/direction/Plot 1/plotter.py
+++ data_plotting/direction/Plot 1/plotter.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from Plot_4LPDA_1dipole_SouthPole import load_file
 import sys
 
@@ -5,7 +7,17 @@ import sys
 i_file = int(sys.argv[1])
 i_event = int(sys.argv[2])
 
+# Loading data
 data = load_file(i_file)
+print(f"Data shape: {data.shape}")
 
-print(data.shape)
-print(data[i_event])
+event_data = data[i_event]
+
+# Plotting
+fig, axs = plt.subplots(5)
+fig.suptitle('Plot of 4 LDPA & 1 dipole')
+
+for i in range(5):
+    axs[i].plot(event_data[i])
+
+plt.savefig(f"plots/plot_file{i_file}_event{i_event}.png")
