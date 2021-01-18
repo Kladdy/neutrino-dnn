fccca3a191609d5f1a5a60e70acf87765c9f22f4
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-18 09:38:59 +0100

    Convert sys.argv to ints before reading

--- data_plotting/direction/Plot 1/plotter.py
+++ data_plotting/direction/Plot 1/plotter.py
@@ -2,8 +2,8 @@ from Plot_4LPDA_1dipole_SouthPole import load_file
 import sys
 
 # Call this function as python plotter.py i_file i_event
-i_file = sys.argv[1]
-i_event = sys.argv[2]
+i_file = int(sys.argv[1])
+i_event = int(sys.argv[2])
 
 data = load_file(i_file)
 
