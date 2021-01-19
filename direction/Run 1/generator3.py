7f9d653281fff95af38e5a8cb171f7e49f1044e5
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-19 08:43:01 +0100

    Update path of data

--- direction/Run 1/generator3.py
+++ direction/Run 1/generator3.py
@@ -5,7 +5,8 @@ import time
 
 np.set_printoptions(precision=4)
 
-datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/Alvarez2009_had_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/"
+#datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/Alvarez2009_had_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/"
+datapath = "/mnt/ssd2/data/energy_reconstruction/ARIANNA-200_Alvarez2000_3sigma_noise/"
 n_files = 394
 # n_files = 10
 n_files_test = 3
