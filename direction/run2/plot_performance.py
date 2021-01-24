a3f25cb67f3f98add795f1b79bbe20328bce616d
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-24 16:16:43 +0100

    Change normalization

--- direction/run2/plot_performance.py
+++ direction/run2/plot_performance.py
@@ -86,7 +86,7 @@ xl = np.linspace(angle.min(), angle.max(), 100) # linspace for plotting
 # fig, ax = php.get_histogram(predicted_nu_energy[:, 0], bins=np.arange(17, 20.1, 0.05), xlabel="predicted energy")
 fig, ax = php.get_histogram(angle, bins=np.linspace(0, 40, 90),
                             xlabel=r"angular difference nu direction")
-ax.plot(xl, stats.rayleigh(scale=scale, loc=loc).pdf(xl))
+ax.plot(xl, N*stats.rayleigh(scale=scale, loc=loc).pdf(xl))
 plt.title(f"Angular resolution for {run_name} with scale={scale} & loc={loc}")
 fig.savefig(f"plots/angular_resolution_{run_name}.png")
 
