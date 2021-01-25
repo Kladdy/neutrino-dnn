c4b5260f5f4d0145352312173902ce6d5836ace8
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-25 15:35:11 +0100

    Add run4

---
+++ direction/run4/constants.py
@@ -0,0 +1,9 @@
+run_version = "run4"
+
+datapath = "/mnt/ssd2/data/energy_reconstruction/ARIANNA-200_Alvarez2000_3sigma_noise/"
+#datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/Alvarez2009_had_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/"
+data_filename = "data_01_LPDA_2of4_3sigma_"
+label_filename = "labels_01_LPDA_2of4_3sigma_"
+
+plots_dir = "plots"
+saved_model_dir = "saved_models"
