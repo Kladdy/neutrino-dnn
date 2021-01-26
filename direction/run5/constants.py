22bcdb45f3d1f5559342ae0e672aaecfe106fe67
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-26 09:34:53 +0100

    Fix run5 name

--- direction/run5/constants.py
+++ direction/run5/constants.py
@@ -1,4 +1,4 @@
-run_version = "runBASE2"
+run_version = "run5"
 
 datapath = "/mnt/ssd2/data/energy_reconstruction/ARIANNA-200_Alvarez2000_3sigma_noise/"
 #datapath = "/mnt/md0/data/SouthPole/single_surface_4LPDA_PA_15m_RNOG_fullsim.json/Alvarez2009_had_noise.yaml/G03generate_events_full_surface_sim/LPDA_2of4_100Hz/4LPDA_1dipole_fullband/"
