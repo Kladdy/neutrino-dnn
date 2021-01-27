9f3dbcb91ec580ee23d9021ef65e4fe52f7a8a4c
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-01-27 21:10:55 +0100

    Move load_file to toolbox file

--- direction/base/base_v4/training.py
+++ direction/base/base_v4/training.py
@@ -15,6 +15,8 @@ import pickle
 import argparse
 from termcolor import colored
 import time
+from toolbox import load_file
+from radiotools import helper as hp
 #from scipy import stats
 from tf_notification_callback import SlackCallback
 import wandb
@@ -216,23 +218,3 @@ print("")
 
 
 
-def load_file(i_file, norm=1e-6):
-#     t0 = time.time()
-#     print(f"loading file {i_file}", flush=True)
-    data = np.load(os.path.join(datapath, f"{data_filename}{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
-    labels_tmp = np.load(os.path.join(datapath, f"{label_filename}{i_file:04d}.npy"), allow_pickle=True)
-#     print(f"finished loading file {i_file} in {time.time() - t0}s")
-    nu_zenith = np.array(labels_tmp.item()["nu_zenith"])
-    nu_azimuth = np.array(labels_tmp.item()["nu_azimuth"])
-    nu_direction = hp.spherical_to_cartesian(nu_zenith, nu_azimuth)
-
-    # check for nans and remove them
-    idx = ~(np.isnan(data))
-    idx = np.all(idx, axis=1)
-    idx = np.all(idx, axis=1)
-    idx = np.all(idx, axis=1)
-    data = data[idx, :, :, :]
-    nu_direction = nu_direction[idx]
-    data /= norm
-
-    return data, nu_direction
