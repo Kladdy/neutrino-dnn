56479dd4346eb4c3fb04b83f0d5437ac8571348e
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-02-01 21:05:29 +0100

    Fix

--- direction/base/base_v5/training.py
+++ direction/base/base_v5/training.py
@@ -38,7 +38,7 @@ from constants import saved_model_dir, run_version, dataset_name, datapath, data
 # -------
 
 # Values
-feedback_freq = 100 # Only train on 1/feedback_freq of data per epoch
+feedback_freq = 50 # Only train on 1/feedback_freq of data per epoch
 webhook = os.getenv("SLACKWEBHOOK")
 architectures_dir = "architectures"
 learning_rate = 0.00005
