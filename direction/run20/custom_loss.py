199bf7d06a49f59ea449034d2d01aae3890032f1
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-02-08 12:19:48 +0100

    Fix custom loss

--- direction/run20/custom_loss.py
+++ direction/run20/custom_loss.py
@@ -4,7 +4,10 @@ import tensorflow.keras.backend as K
 
 def angle_difference_loss(y_true, y_pred):
 
+    y_true_np = K.eval(y_true)
+    y_pred_np = K.eval(y_pred)
+
     for i in range(batch_size):
-        angle_difference_data[i] = hp.get_angle(y_pred[i], y_true[i])
+        angle_difference_data[i] = hp.get_angle(y_pred_np[i], y_true_np[i])
         
     return K.constant(angle_difference_data)
