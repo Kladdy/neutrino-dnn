fb517890d48e68d461c90f1bd6d2acd11963a3ff
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-02-08 12:11:02 +0100

    Fix custom loss fcn

--- direction/run20/custom_loss.py
+++ direction/run20/custom_loss.py
@@ -1,9 +1,10 @@
 from radiotools import helper as hp
 from generator import batch_size
+import tensorflow.keras.backend as K
 
 def angle_difference_loss(y_true, y_pred):
 
     for i in range(batch_size):
         angle_difference_data[i] = hp.get_angle(y_pred[i], y_true[i])
         
-    return angle_difference_data
+    return K.constant(angle_difference_data)
