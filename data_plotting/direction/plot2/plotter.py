c11f157071bfc755526e0bdaa601daa65f9ab2f4
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-02-18 10:08:31 +0100

    Fix plotter

--- data_plotting/direction/plot2/plotter.py
+++ data_plotting/direction/plot2/plotter.py
@@ -33,7 +33,7 @@ direction_data = nu_direction[i_event]
 
 # Getting x axis (1 step is 0.5 ns)
 x_axis_double = range(int(len(event_data[0])))
-x_axis_double = [float(x)/2 for x in x_axis]
+x_axis = [float(x)/2 for x in x_axis_double]
 
 # Plotting
 fig, axs = plt.subplots(5)
