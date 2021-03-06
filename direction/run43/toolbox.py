c6f475199ec6249bce461c5cf9c6a5dd03340aba
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-03-06 20:34:46 +0100

    Add run43

--- direction/run43/toolbox.py
+++ direction/run43/toolbox.py
@@ -10,9 +10,15 @@ import pickle
 
 # Loading data and label files
 def load_file(i_file, norm=1e-6):
+    # Load 500 MHz filter
+    filt = np.load("bandpass_filters/500MHz_filter.npy")
+
 #     t0 = time.time()
 #     print(f"loading file {i_file}", flush=True)
-    data = np.load(os.path.join(datapath, f"{data_filename}{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
+    data = np.load(os.path.join(datapath, f"{data_filename}{i_file:04d}.npy"), allow_pickle=True)
+    data = np.fft.irfft(np.fft.rfft(data, axis=-1) * filt, axis=-1)
+    data = data[:, :, :, np.newaxis]
+
     labels_tmp = np.load(os.path.join(datapath, f"{label_filename}{i_file:04d}.npy"), allow_pickle=True)
 #     print(f"finished loading file {i_file} in {time.time() - t0}s")
     nu_zenith = np.array(labels_tmp.item()["nu_zenith"])
