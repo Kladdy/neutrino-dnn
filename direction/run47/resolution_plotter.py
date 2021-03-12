d7d5b94772c05e67668b889ad9b22d88c318bd4a
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-03-12 08:42:10 +0100

    Add timing to file loading

--- direction/run47/resolution_plotter.py
+++ direction/run47/resolution_plotter.py
@@ -28,11 +28,11 @@ from radiotools import helper as hp
 
 # Loading data and label files and also other properties
 def load_file(i_file, norm=1e-6):
-#     t0 = time.time()
-#     print(f"loading file {i_file}", flush=True)
+    t0 = time.time()
+    print(f"loading file {i_file}", flush=True)
     data = np.load(os.path.join(datapath, f"{data_filename}{i_file:04d}.npy"), allow_pickle=True)[:, :, :, np.newaxis]
     labels_tmp = np.load(os.path.join(datapath, f"{label_filename}{i_file:04d}.npy"), allow_pickle=True)
-#     print(f"finished loading file {i_file} in {time.time() - t0}s")
+    print(f"finished loading file {i_file} in {time.time() - t0}s")
     nu_zenith = np.array(labels_tmp.item()["nu_zenith"])
     nu_azimuth = np.array(labels_tmp.item()["nu_azimuth"])
     nu_direction = hp.spherical_to_cartesian(nu_zenith, nu_azimuth)
