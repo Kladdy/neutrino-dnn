9aa480774fad08530b80d841e4f1b9d5d6c26a0f
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-04-07 09:08:31 +0200

    Add inference tests

---
+++ direction/analysis/inference_tests/dl1_inference_test.py
@@ -0,0 +1,51 @@
+# GPU allocation
+from gpuutils import GpuUtils # pylint: disable=import-error
+GpuUtils.allocate(gpu_count=1, framework='keras')
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+# --------------
+
+# Imports
+import argparse
+import os
+from toolbox import load_file
+from termcolor import cprint
+from tensorflow.keras.models import load_model
+# -------
+
+# Constants
+models_dir = "/mnt/md0/sstjaernholm/neutrino-dnn/final_models"
+plots_dir = "plots"
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Test inference speed on dl1 machine')
+parser.add_argument("run_id", type=str, help="the id of the run, eg '3.2' for run3.2")
+parser.add_argument("i_file", type=int, help="the id file to do inference on")
+
+args = parser.parse_args()
+run_id = args.run_id
+i_file = args.i_file
+
+# Save the run name
+run_name = f"run{run_id}"
+
+# Make sure saved_models folder exists
+if not os.path.exists(plots_dir):
+    os.makedirs(plots_dir)
+
+cprint("Starting inference test for dl1...", "yellow")
+
+# Load model
+model = load_model(f'{models_dir}/model.{run_name}.h5')
+
+data, nu_direction = load_file(i_file)
+
+# Make pedictions and time it
+print(data.shape)
+#nu_direction_predict = model.predict(data)¨
+
+cprint("Inference test for dl1 done!", "green")
+
