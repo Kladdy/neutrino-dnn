7d70e726464759f8fc4183c4b04f31ee3d74df6d
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-04-09 08:38:03 +0200

    Fix

--- direction/tools/to_tflite/convert_to_tflite.py
+++ direction/tools/to_tflite/convert_to_tflite.py
@@ -13,7 +13,7 @@ converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 # Save the model
-with open(f'{model}.run{run_name}.tflite', 'wb') as f:
+with open(f'model.run{run_name}.tflite', 'wb') as f:
     f.write(tflite_model)
 
 print("Done!")
